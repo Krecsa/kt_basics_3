4353b86ff0e4b30c029acdb944a6cad99a7973da
Catherine <malistova02@mail.ru>
2026-01-26 21:02:14 +0300

    Update main (cards).kt

--- kt_basics_3/src/main/kotlin/main.kt
+++ kt_basics_3/src/main/kotlin/main.kt
@@ -1,22 +1,64 @@
 fun main() {
-    val amountRub = 15000 // сумма покупки
-    val regularClient = true
+    val transferAmount = 43579
+    val cardType = "visa" // выбор карты
+    val monthlySpent = 100000 // потрачено в этом месяце
+    val dailySpent = 10000   // потрачено сегодня
 
-    val finalPrice = calculateFinalPrice(amountRub, regularClient)
-    println("Итоговая стоимость покупки: $finalPrice руб.")
+    calculateCommission(transferAmount, cardType, monthlySpent, dailySpent)
 }
 
-fun calculateFinalPrice(amount: Int, regularClient: Boolean): Int {
-    val baseDiscountedAmount = when {
-        amount <= 0 -> 0
-        amount <= 1000 -> amount
-        amount <= 10000 -> amount - 100 // скидка 100 руб.
-        else -> (amount * 0.95).toInt() // 5% скидка
+fun calculateCommission(
+    amount: Int,
+    cardType: String,
+    monthlySpent: Int,
+    dailySpent: Int
+) {
+    if (amount > 150000) {
+        throw IllegalArgumentException("Превышен дневной лимит")
+    }
+    if (monthlySpent + amount > 600000) {
+        throw IllegalArgumentException("Превышен месячный лимит")
     }
 
-    return if (regularClient) {
-        (baseDiscountedAmount * 0.99).toInt()
-    } else {
-        baseDiscountedAmount
+    val dailyTotal = dailySpent + amount
+    val monthlyTotal = monthlySpent + amount
+
+    if (dailyTotal > 150000) {
+        println("Ошибка: превышен дневной лимит $dailyTotal руб.")
+        return
     }
+    if (monthlyTotal > 600000) {
+        println("Ошибка: превышен месячный лимит $monthlyTotal руб.")
+        return
+    }
+
+    var commission = 0
+
+    if (cardType == "mir") {
+        commission = 0 // комиссия не взимается
+    } else if (cardType == "mastercard") {
+        val monthlyLimit = 75000
+        if (monthlySpent>= monthlyLimit) {
+            commission = (amount * 0.006).toInt() + 20
+        } else {
+            if (monthlyTotal <= monthlyLimit) {
+                commission = 0
+            } else {
+                val excessAmount = monthlyTotal - monthlyLimit
+                commission = (excessAmount * 0.006).toInt() + 20
+            }
+        }
+    } else if (cardType == "visa") {
+        val calculated = (amount * 0.0075).toInt()
+        if (calculated < 35) {
+            commission = 35
+        } else {
+            commission = calculated
+        }
+    }
+
+    val totalAmount = amount + commission
+    println("С карты $cardType был осуществлен перевод на сумму $amount руб. ")
+    println("Комиссия составляет: $commission руб.")
+    println("Итого к списанию: $totalAmount руб.")
 }

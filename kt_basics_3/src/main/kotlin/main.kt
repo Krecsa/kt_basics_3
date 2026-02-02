6cf30e50f9a6e5a1a52cc705b75070fb0ac8ebb5
Catherine <malistova02@mail.ru>
2026-02-02 19:26:23 +0300

    when

--- kt_basics_3/src/main/kotlin/main.kt
+++ kt_basics_3/src/main/kotlin/main.kt
@@ -8,10 +8,10 @@ fun main() {
 }
 
 fun calculateCommission(
-    amount: Int,
-    cardType: String,
-    monthlySpent: Int,
-    dailySpent: Int
+    amount: Int = 0,
+    cardType: String = "visa",
+    monthlySpent: Int = 0,
+    dailySpent: Int = 0
 ) {
     if (amount > 150000) {
         throw IllegalArgumentException("Превышен дневной лимит")
@@ -24,26 +24,30 @@ fun calculateCommission(
     val monthlyTotal = monthlySpent + amount
     var commission = 0
 
-    if (cardType == "mir") {
-        commission = 0 // комиссия не взимается
-    } else if (cardType == "mastercard") {
-        val monthlyLimit = 75000
-        if (monthlySpent>= monthlyLimit) {
-            commission = (amount * 0.006).toInt() + 20
-        } else {
-            if (monthlyTotal <= monthlyLimit) {
-                commission = 0
+    when (cardType) {
+        "mir" -> {
+            commission = 0 // комиссия не взимается
+        }
+        "mastercard" -> {
+            val monthlyLimit = 75000
+            if (monthlySpent >= monthlyLimit) {
+                commission = (amount * 0.006).toInt() + 20
             } else {
-                val excessAmount = monthlyTotal - monthlyLimit
-                commission = (excessAmount * 0.006).toInt() + 20
+                if (monthlyTotal <= monthlyLimit) {
+                    commission = 0
+                } else {
+                    val excessAmount = monthlyTotal - monthlyLimit
+                    commission = (excessAmount * 0.006).toInt() + 20
+                }
             }
         }
-    } else if (cardType == "visa") {
-        val calculated = (amount * 0.0075).toInt()
-        if (calculated < 35) {
-            commission = 35
-        } else {
-            commission = calculated
+        "visa" -> {
+            val calculated = (amount * 0.0075).toInt()
+            if (calculated < 35) {
+                commission = 35
+            } else {
+                commission = calculated
+            }
         }
     }
 
@@ -52,4 +56,3 @@ fun calculateCommission(
     println("Комиссия составляет: $commission руб.")
     println("Итого к списанию: $totalAmount руб.")
 }
-
